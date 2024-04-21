24fcd07c2e5cb43a96628af8765fa44fd08b89f8
vosob <osobskyi.vlad@gmail.com>
2024-04-22 01:17:37 +0300

    add /

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <React.StrictMode>
-      <BrowserRouter basename="motorHome">
+      <BrowserRouter basename="/motorHome">
         <App />
       </BrowserRouter>
     </React.StrictMode>
