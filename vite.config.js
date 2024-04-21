5043f9b8ba1aa8d53d284d88570ff649c72a113b
vosob <osobskyi.vlad@gmail.com>
2024-04-22 01:37:27 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      src: '/src',
+      components: '/src/components',
+      pages: '/src/pages',
+      assets: '/src/assets',
+    },
+  },
   base: '/motorHome',
 });
